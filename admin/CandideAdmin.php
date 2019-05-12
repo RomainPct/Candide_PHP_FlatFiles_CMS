a8822bfc38198d78936fe2407d13ecb12220fbc7
Romain Penchenat <romain.penchenat@gmail.com>
2019-05-12 14:00:12 +0200

    Sécurisation compte utilisateur

--- admin/CandideAdmin.php
+++ admin/CandideAdmin.php
@@ -1,10 +1,18 @@
 <?php
 session_start();
 
-if ( key_exists(PROJECT_NAME."_user",$_SESSION)) {
-    echo $_SESSION[PROJECT_NAME."_admin"];
+if ( key_exists(PROJECT_NAME."_logedin",$_SESSION)) {
+    $authorized = false;
+    foreach (ADMINISTRATORS as $user) {
+        if ($_SESSION[PROJECT_NAME."_logedin"] == hash("sha256",$user[0]).hash("sha256",$user[1])) {
+            $authorized = true;
+        }
+    }
+    if (!$authorized) {
+        header("Location: login.php");
+    }
 } else {
-    //header("Location: login.php");
+    header("Location: login.php");
 }
 
 include_once 'php/admin/Administrator.trait.php';
--- admin/login.php
+++ admin/login.php
@@ -2,8 +2,21 @@
 session_start();
 include_once '../Candide.php';
 
-const KEY_USER = PROJECT_NAME."_user";
-const KEY_PASSWORD = PROJECT_NAME."_password";
+if ( !empty($_POST["identifier"]) && !empty($_POST["password"]) ) {
+    foreach (ADMINISTRATORS as $user) {
+        if ($user[0] == $_POST["identifier"]) {
+            if (strtoupper($user[1]) == strtoupper(hash("sha256",$_POST["password"]))) {
+                $_SESSION[PROJECT_NAME."_logedin"] = hash("sha256",$user[0]).hash("sha256",$user[1]);
+                header("Location: index.php");
+            } else {
+                echo "Mot de passe incorrect";
+            }
+        } else {
+            echo "Cet identifiant n'existe pas";
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -19,11 +32,11 @@ const KEY_PASSWORD = PROJECT_NAME."_password";
     <h1>Connexion</h1>
     <div class="inputContainer">
         <h2>Identifiant :</h2>
-        <textarea name='identifier'></textarea>
+        <textarea name='identifier'><?php echo key_exists("identifier",$_POST) ? $_POST["identifier"] : "" ?></textarea>
     </div>
     <div class="inputContainer">
         <h2>Mot de passe :</h2>
-        <textarea name="password"></textarea>
+        <textarea name="password"><?php echo key_exists("password",$_POST) ? $_POST["password"] : "" ?></textarea>
     </div>
     <div class="submitContainer clickable">
         <input type="submit" value="Se connecter">
