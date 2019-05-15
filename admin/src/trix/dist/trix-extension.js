6dee45d22240afcac2daa943aa10ff77db2bbbea
Romain Penchenat <romain.penchenat@gmail.com>
2019-05-15 18:30:21 +0200

    Mise en place Titre1, T2, T3 sur trix

--- admin/src/trix/dist/trix-extension.js
+++ admin/src/trix/dist/trix-extension.js
@@ -1,4 +1,10 @@
 addEventListener("trix-initialize", function(event) {
+    Trix.config.blockAttributes.heading1 = {
+        tagName: "h1",
+        terminal: true,
+        breakOnReturn: true,
+        group: false
+    }
     Trix.config.blockAttributes.heading2 = {
         tagName: "h2",
         terminal: true,
@@ -12,12 +18,15 @@ addEventListener("trix-initialize", function(event) {
         group: false
     }
 
-    let h2ButtonHTML = newButton("heading2","heading","Titre")
-    let h3ButtonHTML = newButton("heading3","heading","Sous-titre")
+    let h1ButtonHTML = newButton("heading1","heading","Titre 1")
+    let h2ButtonHTML = newButton("heading2","heading","T2")
+    let h3ButtonHTML = newButton("heading3","heading","T3")
     const groupElement = event.target.toolbarElement.querySelectorAll('.trix-button-group--block-tools')[0];
 
-    groupElement.insertBefore(h2ButtonHTML,groupElement.children[0])
-    groupElement.insertBefore(h3ButtonHTML,groupElement.children[1])
+    groupElement.insertBefore(h1ButtonHTML,groupElement.children[0])
+    groupElement.insertBefore(h2ButtonHTML,groupElement.children[1])
+    groupElement.insertBefore(h3ButtonHTML,groupElement.children[2])
+    groupElement.children[3].remove()
 })
 
 function newButton(trixAttribute,title,text) {
