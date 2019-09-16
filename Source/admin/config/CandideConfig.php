5f83d1e5edd3eaadd7dd039774089ff64ac54981
Romain Penchenat <romain.penchenat@gmail.com>
2019-09-16 22:31:12 +0200

    Add french traduction

--- Source/admin/config/CandideConfig.php
+++ Source/admin/config/CandideConfig.php
@@ -3,7 +3,7 @@ const DEV_MODE = true;
 const WELCOME_TITLE = "Bienvenue sur votre espace administrateur Candide";
 const WELCOME_PARAGRAPH = "Vous pouvez modifier le contenu de vos pages via le menu de gauche.";
 const PROJECT_NAME = "candide_project";
-const LOCALE = "en"; // LCID String https://www.science.co.il/language/Locale-codes.php
+const LOCALE = "fr-fr"; // LCID String https://www.science.co.il/language/Locale-codes.php
 const ADMINISTRATORS = [
     ["admin","4194D1706ED1F408D5E02D672777019F4D5385C766A8C6CA8ACBA3167D36A7B9"] // administrator
 ];
