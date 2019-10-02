3d31c3d60c2d5d5b3c00c7793a72ac63150d4f3f
Romain Penchenat <romain.penchenat@gmail.com>
2019-10-02 13:56:31 +0200

    Cleaning code

--- Source/admin/php/actions/reorderCollectionItem.php
+++ Source/admin/php/actions/reorderCollectionItem.php
@@ -4,4 +4,3 @@ set_time_limit(0);
 include '../../CandideAdmin.php';
 $manager = new CandideCollectionReorder($_POST["candide_instance_name"]);
 $newIds = $manager->reorder($_POST["reorderedItemIndex"],$_POST["insertBeforeIndex"]);
-echo json_encode($newIds);
