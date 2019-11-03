4c30dde071c6b43ae05550904be4c3de06e7cf75
Romain Penchenat <romain.penchenat@gmail.com>
2019-11-03 13:08:15 +0100

    Improved development environment

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,10 @@ var gulpfile = require('gulp');
 var plugins = require('gulp-load-plugins')(); // tous les plugins de package.json
 
 // Variables de chemins
-var source = 'src/'; // dossier de travail
-var destination = 'assets/'; // dossier à livrer
-const jsSource = 'src/js/',
-    jsDestination = 'assets/scripts/'
+const cssSource = 'Source/admin/src/scss/',
+      cssDestination = 'Source/admin/assets/styles/',
+      jsSource = 'Source/admin/src/js/',
+      jsDestination = 'Source/admin/assets/scripts/'
 
 gulpfile.task('jsMinifier', function(done) {
     gulpfile.src(jsSource+"*.js") // path to your files
@@ -23,34 +23,34 @@ gulpfile.task('jsMinifier', function(done) {
     done()
 });
 
-gulpfile.task('jsLint', function(done) {
-    gulpfile.src(jsSource+"/**/*.js") // path to your files
-        .pipe(plugins.jshint())
-        .pipe(plugins.jshint.reporter()); // Dump results
-    done()
-})
+// gulpfile.task('jsLint', function(done) {
+//     gulpfile.src(jsSource+"/**/*.js") // path to your files
+//         .pipe(plugins.jshint())
+//         .pipe(plugins.jshint.reporter()); // Dump results
+//     done()
+// })
 
 gulpfile.task('sass', function() {
-    return gulpfile.src(source + 'scss/main.scss')
+    return gulpfile.src(cssSource + 'main.scss')
         .pipe(plugins.sass().on('error', plugins.sass.logError))
         .pipe(plugins.autoprefixer())
-        .pipe(gulpfile.dest(destination + 'styles/'))
+        .pipe(gulpfile.dest(cssDestination))
 });
 
 gulpfile.task('cssMinfier', function() {
-    return gulpfile.src(destination + 'styles/main.css')
+    return gulpfile.src(cssDestination + 'main.css')
         .pipe(plugins.csso())
         .pipe(plugins.rename({
             'suffix': '.min'
         }))
-        .pipe(gulpfile.dest(destination + 'styles/'))
+        .pipe(gulpfile.dest(cssDestination))
 });
 
 gulpfile.task('js', gulpfile.series('jsMinifier'))
 gulpfile.task('css', gulpfile.series('sass', 'cssMinfier'))
 gulpfile.task('watch', function() {
-    gulpfile.watch('./src/js/*.js', gulpfile.series('js'))
-    gulpfile.watch('./src/scss/**/*.scss', gulpfile.series('css'))
+    gulpfile.watch('./Source/admin/src/js/*.js', gulpfile.series('js'))
+    gulpfile.watch('./Source/admin/src/scss/**/*.scss', gulpfile.series('css'))
 });
 
 gulpfile.task('default', gulpfile.series('js','css','watch'));
